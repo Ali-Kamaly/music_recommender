93b826fd30d406c69da334dce3eb38a1ca27a795
Ali Kamaly <alikamaly07@gmail.com>
2026-06-16 00:02:17 +0100

    Implement preprocessing pipeline and feature standardisation

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import load_data
 
+from sklearn.preprocessing import StandardScaler
+
 def clean_data():
     original_df = load_data.get_df()
 
@@ -11,6 +13,36 @@ def clean_data():
     #there are repeated track_ids meaning duplicate songs 
     new_df.dropna(inplace = True)
     new_df.drop_duplicates(subset = 'track_id', inplace = True)
+    new_df = new_df.drop(columns = 'track_id')
+    #track id now redundant after removing duplicates
 
     print(original_df)
     print(new_df)
+    return new_df
+
+#standardisation vs normalisation
+"""
+standardisation = normal distribution with mean = 0 std = 1
+normalisation = scale 0 - 1
+chose standardisation as there may be outliers more on reasoning later
+
+"""
+def standardise_data(new_df):
+    continuous_attributes = ['danceability','energy','loudness','speechiness','acousticness','instrumentalness',
+                'liveness','valence','tempo']
+    scaler = StandardScaler()
+    new_df[continuous_attributes] = scaler.fit_transform(new_df[continuous_attributes])
+    #standardising only relevant continuous numerical attributes
+    print(new_df)
+    return new_df
+
+def save_df(df):
+    df.to_csv('data\processed\processed_dataset.csv')
+
+
+def preprocess():
+    new_df = clean_data()
+    preprocessed_df = standardise_data(new_df)
+    save_df(preprocessed_df)
+
+preprocess()
